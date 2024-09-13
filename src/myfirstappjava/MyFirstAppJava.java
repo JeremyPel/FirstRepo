0e0dca4588ac81e752ed71998dba549fbf5a0af1
JeremyPel <pellazar@sheridancollege.ca>
2024-09-13 10:03:47 -0400

    Very first commit woohoo

---
+++ src/myfirstappjava/MyFirstAppJava.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package myfirstappjava;
+
+/**
+ * This CLass:
+ * @author Jeremy Pellazar
+ */
+public class MyFirstAppJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello there"); //making a change
+    }
+
+}
